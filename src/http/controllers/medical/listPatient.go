9cf0c953e85f25deef214184d65b2bba93664922
Dion Fananie <dionfananie@gmail.com>
2024-05-14 06:08:33 +0700

    feat(register): controller

--- src/http/controllers/medical/listPatient.go
+++ src/http/controllers/medical/listPatient.go
@@ -2,7 +2,7 @@ package medicalController
 
 import "github.com/labstack/echo/v4"
 
-func (dbase *V1Medical) MedicalRegister(c *echo.Context) {
+func (dbase *V1Medical) PatientList(c *echo.Context) {
 	// var req user.IT
 
 	// c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully", "data": gin.H{
