f55532b7ccef9a32991cfd60680bac5f01fda935
Dion Fananie <dionfananie@gmail.com>
2024-05-14 06:05:07 +0700

    feat(register): fix dbase

--- src/http/models/user/nurse/register.go
+++ src/http/models/user/nurse/register.go
@@ -3,5 +3,5 @@ package user
 type NurseRegister struct {
 	Nip                 string `json:"nip" binding:"required,min=5,max=13"`
 	Name                string `json:"name" binding:"required,min=5,max=50"`
-	IdentityCardScanImg string `json:"identity_image" binding:"required,min=2,max=5"`
+	IdentityCardScanImg string `json:"identity_image" binding:"required"`
 }
