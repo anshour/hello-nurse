60de3a5310cb22620948ae5c1707d3d8e7938913
Anshor J <lordlexa10@gmail.com>
2024-05-15 10:05:01 +0700

    feat(validation) : change struc tag to validate

--- src/http/models/user/nurse/register.go
+++ src/http/models/user/nurse/register.go
@@ -1,7 +1,7 @@
 package user
 
 type NurseRegister struct {
-	Nip                 int64  `json:"nip" binding:"required"`
-	Name                string `json:"name" binding:"required,min=5,max=50"`
-	IdentityCardScanImg string `json:"identity_image" binding:"required"`
+	Nip                 int64  `json:"nip" validate:"required"`
+	Name                string `json:"name" validate:"required,min=5,max=50"`
+	IdentityCardScanImg string `json:"identity_image" validate:"required"`
 }
