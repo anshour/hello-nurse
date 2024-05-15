1b4f7661d5d3f5ea6773790f20f274ebec7cc876
Anshor J <lordlexa10@gmail.com>
2024-05-15 17:22:13 +0700

    feat(users): fix wrong struct tag

--- src/http/models/user/nurse/register.go
+++ src/http/models/user/nurse/register.go
@@ -1,7 +1,7 @@
 package user
 
 type NurseRegister struct {
-	Nip                 int64  `json:"nip" validate:"required"`
-	Name                string `json:"name" validate:"required,min=5,max=50"`
-	IdentityCardScanImg string `json:"identity_image" validate:"required"`
+	Nip           int64  `json:"nip" validate:"required"`
+	Name          string `json:"name" validate:"required,min=5,max=50"`
+	IdentityImage string `json:"identityCardScanImg" validate:"required"`
 }
