c9e6e026c6f86d2236c40018ebb3588703396636
Anshor J <lordlexa10@gmail.com>
2024-05-15 16:43:47 +0700

    feat(users) : max password length 33

--- src/http/models/user/it/register.go
+++ src/http/models/user/it/register.go
@@ -3,5 +3,5 @@ package user
 type ITRegister struct {
 	Nip      int64  `json:"nip" validate:"required"`
 	Name     string `json:"name" validate:"required,min=5,max=50"`
-	Password string `json:"password" validate:"required,min=5,max=15"`
+	Password string `json:"password" validate:"required,min=5,max=33"`
 }
