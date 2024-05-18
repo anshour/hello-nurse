b4a767ff6d0718aa31e5a61617340df6e983832b
Anshor J <lordlexa10@gmail.com>
2024-05-18 14:25:22 +0700

    feat(auth): remove unintended println

--- src/utils/middlewares/onlyRole.go
+++ src/utils/middlewares/onlyRole.go
@@ -10,7 +10,6 @@ func OnlyRole(role string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			userRole, _ := c.Get("role").(string)
-			println("masuk role", userRole, role)
 			if userRole != role {
 				return c.JSON(http.StatusUnauthorized, ErrorResponse{
 					Status:  false,
