97dbb6c1e125bacdbce9113ecf54cb9adbaede3e
Anshor J <lordlexa10@gmail.com>
2024-05-18 22:49:30 +0700

    feat(error) : Add error conflict

--- src/constants/error.go
+++ src/constants/error.go
@@ -3,4 +3,5 @@ package constants
 import "errors"
 
 var ErrNotFound = errors.New("no row affected")
-var ErrInternalServer = errors.New("rrror checking row affected")
+var ErrConflict = errors.New("error conflict")
+var ErrInternalServer = errors.New("errror checking row affected")
