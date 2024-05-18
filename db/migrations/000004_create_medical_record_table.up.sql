792cc98920d09e677bcc9dda8eb3eb9a157d019b
Anshor J <lordlexa10@gmail.com>
2024-05-18 23:26:54 +0700

    feat(record): patient can have multiple records

--- db/migrations/000004_create_medical_record_table.up.sql
+++ db/migrations/000004_create_medical_record_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS medical_records (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    identity_number BIGINT NOT NULL UNIQUE,
+    identity_number BIGINT NOT NULL,
     FOREIGN KEY (identity_number) REFERENCES patients(identity_number),
     user_id uuid NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
