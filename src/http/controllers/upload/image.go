8c058c8dcd0b1ed6579068b52ff6562ead929132
Anshor J <lordlexa10@gmail.com>
2024-05-19 10:14:01 +0700

    feat(upload): upload file to s3

--- src/http/controllers/upload/image.go
+++ src/http/controllers/upload/image.go
@@ -1,42 +1,109 @@
 package uploadController
 
 import (
-	"io"
+	"fmt"
+	"hello-nurse/src/config"
 	"net/http"
-	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
+type UploadImageResponse struct {
+	ImageUrl string `json:"imageUrl"`
+}
+
 func (dbase *V1Upload) UploadImage(c echo.Context) (err error) {
+	const minSize int64 = 10 * 1024       // 10KB
+	const maxSize int64 = 2 * 1024 * 1024 // 2MB
+
 	// Read form file
 	file, err := c.FormFile("file")
-	println(err.Error())
 	if err != nil {
 		return err
 	}
+
+	if file.Size < minSize {
+		return c.JSON(http.StatusBadRequest, ErrorResponse{
+			Status:  false,
+			Message: "file size minimum 10KB",
+		})
+	}
+	if file.Size > maxSize {
+		return c.JSON(http.StatusBadRequest, ErrorResponse{
+			Status:  false,
+			Message: "file size maximum 2MB",
+		})
+	}
+
+	if file.Size == 0 {
+		return c.JSON(http.StatusBadRequest, ErrorResponse{
+			Status:  false,
+			Message: "file is empty",
+		})
+	}
+
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+	if ext != ".jpg" && ext != ".jpeg" {
+		return c.JSON(http.StatusBadRequest, ErrorResponse{
+			Status:  false,
+			Message: "file must be a .jpg or .jpeg",
+		})
+	}
+
 	// Source
 	src, err := file.Open()
 	if err != nil {
 		return err
 	}
+
 	defer src.Close()
 
-	// Destination
-	dst, err := os.Create(file.Filename)
-	if err != nil {
-		return err
-	}
-	defer dst.Close()
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(config.AWS_REGION),
+		Credentials: credentials.NewStaticCredentials(
+			config.AWS_ACCESS_KEY_ID,
+			config.AWS_SECRET_ACCESS_KEY,
+			"",
+		),
+	})
 
-	// Copy
-	if _, err = io.Copy(dst, src); err != nil {
-		return err
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, ErrorResponse{
+			Status:  false,
+			Message: err.Error(),
+		})
 	}
+
+	uploader := s3manager.NewUploader(sess)
+	newFilename := uuid.New().String() + ext
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(config.AWS_BUCKET),
+		Key:    aws.String(newFilename),
+		Body:   src,
+	})
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, ErrorResponse{
+			Status:  false,
+			Message: err.Error(),
+		})
+	}
+
+	fileURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", config.AWS_BUCKET, config.AWS_REGION, newFilename)
 
 	return c.JSON(http.StatusCreated, SuccessResponse{
-		Message: "Success",
-		Data:    "",
+		Message: "File uploaded sucessfully",
+		Data: UploadImageResponse{
+			ImageUrl: fileURL,
+		},
 	})
 
 }
